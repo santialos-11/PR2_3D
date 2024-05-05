f3cb1877ff753d58287369aac00521aee840afcd
santialos-11 <santialos11@gmail.com>
2024-05-05 17:01:08 +0200

    pr2 final

---
+++ Assets/scripts/DanzaFinal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanzaFinal : MonoBehaviour
+{
+    Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)){
+            _animator.SetTrigger("DanzaFinal");
+        }
+    }
+}
